Jmalinza/pytorch

Large Model Support

aten/src/ATen/function_wrapper.py
@@ -50,7 +50,7 @@ ${return_type} ${api_name}(${type_method_formals}) {
 #ifdef BUILD_NAMEDTENSOR
     ${named_guard_declaration}
 #endif
-    ${device_guard_declaration}
+    ${device_guard_declarations}
     Tensor ${broadcast_returns};
     std::tie(${broadcast_returns}) = ${broadcast_function}(${broadcast_actuals}, "${api_name}");
     return ${method_prefix_derived}${api_name}(${broadcast_modified_actuals});
@@ -65,7 +65,7 @@ ${return_type} ${method_prefix_derived}${api_name}(${type_method_formals}) {
 #ifdef BUILD_NAMEDTENSOR
     ${named_guard_declaration}
 #endif
-    ${device_guard_declaration}
+    ${device_guard_declarations}
     ${type_definition_body}
 }
 """)
@@ -97,7 +97,7 @@ ${return_type} TypeDefault::${api_name}(${type_method_formals}) {
 #ifdef BUILD_NAMEDTENSOR
     ${named_guard_declaration}
 #endif
-    ${device_guard_declaration}
+    ${device_guard_declarations}
     ${return_call} at::native::${native_type_method_dispatch}(${native_actuals});
 }
 """)
@@ -107,7 +107,7 @@ ${return_type} ${Type}::${api_name}(${type_method_formals}) {
 #ifdef BUILD_NAMEDTENSOR
     ${named_guard_declaration}
 #endif
-    ${device_guard_declaration}
+    ${device_guard_declarations}
     ${return_call} at::native::${native_type_method_dispatch}(${native_actuals});
 }
 """)
@@ -511,7 +511,7 @@ FunctionOption = TypedDict('FunctionOption', {
     'condition': str,
     'const_mark': str,
     'device_guard': bool,
-    'device_guard_declaration': str,
+    'device_guard_declarations': List[str],
     'dispatch_scalar_type_declaration': str,
     'with_gil': bool,
     'cpu_half': bool,
@@ -592,14 +592,18 @@ FunctionCode = NamedTuple('FunctionCode', [
 ])
 
 
-def device_guard(option, dispatch_options, dispatch_tensor):
+def device_guards(option, dispatch_options, dispatch_tensor, formals):
     # For factory methods the `DeviceGuard` is already in the template.
     if option.get('device_guard', True):
+        code = []
         if dispatch_options:
-            return 'const DeviceGuard device_guard({}.device());'.format(dispatch_options['name'])
-        if dispatch_tensor:
-            return 'const OptionalDeviceGuard device_guard(device_of({}));'.format(dispatch_tensor)
-    return '// DeviceGuard omitted'
+            code.append('const DeviceGuard device_guard({}.device());'.format(dispatch_options['name']))
+        elif dispatch_tensor:
+            code.append('const OptionalDeviceGuard device_guard(device_of({}));'.format(dispatch_tensor))
+        for arg in [f for f in formals if f['dynamic_type'] in {'Tensor', 'TensorList'}]:
+            code.append('const {0}Guard {1}_tensor_guard({1});'.format(arg['dynamic_type'], arg['name']))
+        return code
+    return ['// DeviceGuard omitted']
 
 
 def named_guard(option, tensors, tensorlists):
@@ -869,6 +873,7 @@ def create_generic(top_env, declarations):
             # arguments list between output and input arguments
             for buffer in option['buffers']:
                 body.append('Tensor {} = at::empty({{0}}, this->options());'.format(buffer['name']))
+                body.append('const TensorGuard {0}_tensor_guard({0});'.format(buffer['name']))
             actuals = [arg['name'] for arg in option['arguments'] if arg.get('output')]
             actuals += [buffer['name'] for buffer in option['buffers']]
             actuals += [arg['name'] for arg in option['arguments'] if not arg.get('output')]
@@ -914,7 +919,7 @@ def create_generic(top_env, declarations):
         option['method_prefix_derived'] = '' if broadcast_arg is None else 's_'
         if option['mode'] == 'TH':
             option['device_guard'] = False
-        option['device_guard_declaration'] = device_guard(option, False, dispatch_tensor)
+        option['device_guard_declarations'] = device_guards(option, False, dispatch_tensor, formals)
         option['named_guard_declaration'] = named_guard(option, find_tensors(formals),
                                                         find_tensorlists(formals))
         option['dispatch_scalar_type_declaration'] = dispatch_scalar_type(option, False, dispatch_tensor)
@@ -1132,7 +1137,7 @@ def create_generic(top_env, declarations):
         check_methods_do_not_start_with_underscore(option['name'], is_method)
 
         option['method_prefix_derived'] = ''
-        option['device_guard_declaration'] = device_guard(option, dispatch_options, dispatch_tensor)
+        option['device_guard_declarations'] = device_guards(option, dispatch_options, dispatch_tensor, formals)
         option['named_guard_declaration'] = named_guard(option, find_tensors(formals),
                                                         find_tensorlists(formals))
         option['dispatch_scalar_type_declaration'] = dispatch_scalar_type(option, dispatch_options, dispatch_tensor)
@@ -1356,10 +1361,14 @@ def create_derived(backend_type_env, declarations):
             tensor_arg = ('{}_ == nullptr ? (TensorImpl*)UndefinedTensorImpl::singleton() : (TensorImpl*){}_'
                           .format(name, name))
         intrusive_ptr_type = 'c10::intrusive_ptr<TensorImpl, UndefinedTensorImpl>'
-        return [
+        code = [
             'auto {}_ = {};'.format(name, allocation),
             'auto {} = Tensor({}::reclaim({}));'.format(name, intrusive_ptr_type, tensor_arg),
         ]
+        if is_cuda:
+            code.append('const TensorGuard {0}_tensor_guard({0});'.format(name))
+        return code
+
 
     def resize_arg(arg):
         # type: (THFormal) -> str

c10/cuda/CUDAStream.cpp
@@ -73,6 +73,13 @@ static std::array<LeakyStreamInternals, kStreamsPerPool>
 static std::array<LeakyStreamInternals, kStreamsPerPool>
     high_priority_streams[C10_COMPILE_TIME_MAX_GPUS];
 
+// LMS streams
+static constexpr unsigned int kLMSFlags = cudaStreamDefault;
+static std::once_flag device_flags_lms[C10_COMPILE_TIME_MAX_GPUS];
+static std::atomic<uint32_t> lms_counters[C10_COMPILE_TIME_MAX_GPUS];
+static std::array<LeakyStreamInternals, kStreamsPerPool>
+    lms_streams[C10_COMPILE_TIME_MAX_GPUS];
+
 // Note [StreamId assignment]
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~
 // How do we assign stream IDs?
@@ -84,6 +91,7 @@ static std::array<LeakyStreamInternals, kStreamsPerPool>
 //  00 = default stream
 //  01 = low priority stream
 //  10 = high priority stream
+//  11 = LMS stream
 //
 // This is not really for efficiency; it's just easier to write the code
 // to extract the index if we do this with bitmasks :)
@@ -104,6 +112,7 @@ enum class StreamIdType : uint8_t {
   DEFAULT = 0x0,
   LOW = 0x1,
   HIGH = 0x2,
+  LMS = 0x3,
 };
 
 std::ostream& operator<<(std::ostream& stream, StreamIdType s) {
@@ -116,6 +125,9 @@ std::ostream& operator<<(std::ostream& stream, StreamIdType s) {
       break;
     case StreamIdType::HIGH:
       stream << "HIGH";
+      break;
+    case StreamIdType::LMS:
+      stream << "LMS";
       break;
     default:
       stream << static_cast<uint8_t>(s);
@@ -176,6 +188,13 @@ static StreamId CUDAStream_getStreamId(const LeakyStreamInternals* ptr) {
           ptr, high_priority_streams[device_index])) {
     return makeStreamId(
         StreamIdType::HIGH, ptr - high_priority_streams[device_index].data());
+  }
+
+  // Check if it's a LMS stream
+  if (pointer_within<LeakyStreamInternals>(
+          ptr, lms_streams[device_index])) {
+    return makeStreamId(
+        StreamIdType::LMS, ptr - lms_streams[device_index].data());
   }
 
   AT_ASSERTM(
@@ -243,6 +262,21 @@ static void initDeviceStreamState(DeviceIndex device_index) {
   }
 }
 
+// Creates the LMS stream pools for the specified device
+// Warning: only call once per device!
+static void initDeviceLMSStreamState(DeviceIndex device_index) {
+  // Switches to the requested device so streams are properly associated
+  // with it.
+  CUDAGuard device_guard{device_index};
+
+  for (auto i = decltype(kStreamsPerPool){0}; i < kStreamsPerPool; ++i) {
+    auto& stream = lms_streams[device_index][i];
+
+    stream.device_index = device_index;
+    C10_CUDA_CHECK(cudaStreamCreateWithFlags(&stream.stream, kLMSFlags));
+  }
+}
+
 // Init front-end to ensure initialization only occurs once
 static void initCUDAStreamsOnce() {
   // Inits default streams (once, globally)
@@ -293,6 +327,8 @@ LeakyStreamInternals* CUDAStream_internals(CUDAStream s) {
       return &low_priority_streams[device_index][si];
     case StreamIdType::HIGH:
       return &high_priority_streams[device_index][si];
+    case StreamIdType::LMS:
+      return &lms_streams[device_index][si];
     default:
       AT_ASSERTM(
           0,
@@ -367,6 +403,20 @@ void setCurrentCUDAStream(CUDAStream stream) {
   auto ptr = CUDAStream_internals(stream);
   AT_ASSERT(ptr);
   current_streams[ptr->device_index] = ptr;
+}
+
+CUDAStream getLMSCUDAStream(DeviceIndex device_index) {
+  initCUDAStreamsOnce();
+  if (device_index == -1)
+    device_index = current_device();
+  check_gpu(device_index);
+
+  // Initializes the LMS stream pool (once)
+  std::call_once(
+      device_flags_lms[device_index], initDeviceLMSStreamState, device_index);
+
+  const auto idx = get_idx(lms_counters[device_index]);
+  return CUDAStream_fromInternals(&lms_streams[device_index][idx]);
 }
 
 std::ostream& operator<<(std::ostream& stream, const CUDAStream& s) {

mcrl/pytorch-lms

aten/src/ATen/function_wrapper.py
@@ -46,7 +46,7 @@ ${return_type} ${api_name}(${type_method_formals});
 LEGACY_TH_DEFINITION_BROADCAST = CodeTemplate("""\
 ${return_type} ${api_name}(${type_method_formals}) {
     ${named_guard_declaration}
-    ${device_guard_declaration}
+    ${device_guard_declarations}
     Tensor ${broadcast_returns};
     std::tie(${broadcast_returns}) = ${broadcast_function}(${broadcast_actuals}, "${api_name}");
     return ${method_prefix_derived}${api_name}(${broadcast_modified_actuals});
@@ -59,7 +59,7 @@ ${return_type} ${method_prefix_derived}${api_name}(${type_method_formals});
 LEGACY_TH_DEFINITION = CodeTemplate("""\
 ${return_type} ${method_prefix_derived}${api_name}(${type_method_formals}) {
     ${named_guard_declaration}
-    ${device_guard_declaration}
+    ${device_guard_declarations}
     ${type_definition_body}
 }
 """)
@@ -90,7 +90,7 @@ ${return_type} ${type_wrapper_name}(${type_method_formals});
 NATIVE_DISPATCH_DEFINITION_DEFAULT = CodeTemplate("""\
 ${return_type} ${type_wrapper_name}(${type_method_formals}) {
     ${named_guard_declaration}
-    ${device_guard_declaration}
+    ${device_guard_declarations}
     ${return_call} at::native::${native_type_method_dispatch}(${native_actuals});
 }
 """)
@@ -98,7 +98,7 @@ ${return_type} ${type_wrapper_name}(${type_method_formals}) {
 NATIVE_DISPATCH_DEFINITION_BACKEND = CodeTemplate("""\
 ${return_type} ${type_wrapper_name}(${type_method_formals}) {
     ${named_guard_declaration}
-    ${device_guard_declaration}
+    ${device_guard_declarations}
     ${return_call} at::native::${native_type_method_dispatch}(${native_actuals});
 }
 """)
@@ -500,7 +500,7 @@ FunctionOption = TypedDict('FunctionOption', {
     'category_override': str,
     'condition': str,
     'device_guard': bool,
-    'device_guard_declaration': str,
+    'device_guard_declarations': List[str],
     'dispatch_scalar_type_declaration': str,
     'use_c10_dispatcher': str,
     'manual_kernel_registration': bool,
@@ -590,14 +590,18 @@ OpRegistration = NamedTuple('OpRegistration', [
 ])
 
 
-def device_guard(option, dispatch_options, dispatch_tensor):
+def device_guards(option, dispatch_options, dispatch_tensor, formals):
     # For factory methods the `DeviceGuard` is already in the template.
     if option.get('device_guard', True):
+        code = []
         if dispatch_options:
-            return 'const DeviceGuard device_guard({}.device());'.format(dispatch_options['name'])
-        if dispatch_tensor:
-            return 'const OptionalDeviceGuard device_guard(device_of({}));'.format(dispatch_tensor)
-    return '// DeviceGuard omitted'
+            code.append('const DeviceGuard device_guard({}.device());'.format(dispatch_options['name']))
+        elif dispatch_tensor:
+            code.append('const OptionalDeviceGuard device_guard(device_of({}));'.format(dispatch_tensor))
+        for arg in [f for f in formals if f['dynamic_type'] in {'Tensor', 'TensorList'}]:
+            code.append('const {0}Guard {1}_tensor_guard({1});'.format(arg['dynamic_type'], arg['name']))
+        return code
+    return ['// DeviceGuard omitted']
 
 
 def named_guard(option, tensors, tensorlists):
@@ -915,7 +919,7 @@ def create_generic(top_env, declarations):
         option['method_prefix_derived'] = '' if broadcast_arg is None else 's_'
         if option['mode'] == 'TH':
             option['device_guard'] = False
-        option['device_guard_declaration'] = device_guard(option, False, dispatch_tensor)
+        option['device_guard_declarations'] = device_guards(option, False, dispatch_tensor, formals)
         option['named_guard_declaration'] = named_guard(option, find_tensors(formals),
                                                         find_tensorlists(formals))
         option['dispatch_scalar_type_declaration'] = dispatch_scalar_type(option, False, dispatch_tensor)
@@ -1181,7 +1185,7 @@ def create_generic(top_env, declarations):
         # device guard and then manually add the guards you need.
         dispatch_options = find_formal('TensorOptions', formals)
         guard_tensor = None if dispatch_options else find_dispatch_tensor(formals)
-        option['device_guard_declaration'] = device_guard(option, dispatch_options, guard_tensor)
+        option['device_guard_declarations'] = device_guards(option, dispatch_options, guard_tensor, formals)
         option['named_guard_declaration'] = named_guard(option, find_tensors(formals),
                                                         find_tensorlists(formals))
         option['dispatch_scalar_type_declaration'] = dispatch_scalar_type(option, dispatch_options, guard_tensor)
@@ -1372,10 +1376,14 @@ def create_derived(backend_type_env, declarations):
             tensor_arg = ('{}_ == nullptr ? (TensorImpl*)UndefinedTensorImpl::singleton() : (TensorImpl*){}_'
                           .format(name, name))
         intrusive_ptr_type = 'c10::intrusive_ptr<TensorImpl, UndefinedTensorImpl>'
-        return [
+        code = [
             'auto {}_ = {};'.format(name, allocation),
             'auto {} = Tensor({}::reclaim({}));'.format(name, intrusive_ptr_type, tensor_arg),
         ]
+        if is_cuda:
+            code.append('const TensorGuard {0}_tensor_guard({0});'.format(name))
+        return code
+
 
     def resize_arg(arg):
         # type: (THFormal) -> str

c10/cuda/CUDAStream.cpp
@@ -73,6 +73,13 @@ static std::array<LeakyStreamInternals, kStreamsPerPool>
 static std::array<LeakyStreamInternals, kStreamsPerPool>
     high_priority_streams[C10_COMPILE_TIME_MAX_GPUS];
 
+// LMS streams
+static constexpr unsigned int kLMSFlags = cudaStreamNonBlocking;
+static std::once_flag device_flags_lms[C10_COMPILE_TIME_MAX_GPUS];
+static std::atomic<uint32_t> lms_counters[C10_COMPILE_TIME_MAX_GPUS];
+static std::array<LeakyStreamInternals, kStreamsPerPool>
+    lms_streams[C10_COMPILE_TIME_MAX_GPUS];
+
 // Note [StreamId assignment]
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~
 // How do we assign stream IDs?
@@ -84,6 +91,7 @@ static std::array<LeakyStreamInternals, kStreamsPerPool>
 //  00 = default stream
 //  01 = low priority stream
 //  10 = high priority stream
+//  11 = LMS stream
 //
 // This is not really for efficiency; it's just easier to write the code
 // to extract the index if we do this with bitmasks :)
@@ -104,6 +112,7 @@ enum class StreamIdType : uint8_t {
   DEFAULT = 0x0,
   LOW = 0x1,
   HIGH = 0x2,
+  LMS = 0x3,
 };
 
 std::ostream& operator<<(std::ostream& stream, StreamIdType s) {
@@ -116,6 +125,9 @@ std::ostream& operator<<(std::ostream& stream, StreamIdType s) {
       break;
     case StreamIdType::HIGH:
       stream << "HIGH";
+      break;
+    case StreamIdType::LMS:
+      stream << "LMS";
       break;
     default:
       stream << static_cast<uint8_t>(s);
@@ -176,6 +188,13 @@ static StreamId CUDAStream_getStreamId(const LeakyStreamInternals* ptr) {
           ptr, high_priority_streams[device_index])) {
     return makeStreamId(
         StreamIdType::HIGH, ptr - high_priority_streams[device_index].data());
+  }
+
+  // Check if it's a LMS stream
+  if (pointer_within<LeakyStreamInternals>(
+          ptr, lms_streams[device_index])) {
+    return makeStreamId(
+        StreamIdType::LMS, ptr - lms_streams[device_index].data());
   }
 
   AT_ASSERTM(
@@ -243,6 +262,21 @@ static void initDeviceStreamState(DeviceIndex device_index) {
   }
 }
 
+// Creates the LMS stream pools for the specified device
+// Warning: only call once per device!
+static void initDeviceLMSStreamState(DeviceIndex device_index) {
+  // Switches to the requested device so streams are properly associated
+  // with it.
+  CUDAGuard device_guard{device_index};
+
+  for (auto i = decltype(kStreamsPerPool){0}; i < kStreamsPerPool; ++i) {
+    auto& stream = lms_streams[device_index][i];
+
+    stream.device_index = device_index;
+    C10_CUDA_CHECK(cudaStreamCreateWithFlags(&stream.stream, kLMSFlags));
+  }
+}
+
 // Init front-end to ensure initialization only occurs once
 static void initCUDAStreamsOnce() {
   // Inits default streams (once, globally)
@@ -293,6 +327,8 @@ LeakyStreamInternals* CUDAStream_internals(CUDAStream s) {
       return &low_priority_streams[device_index][si];
     case StreamIdType::HIGH:
       return &high_priority_streams[device_index][si];
+    case StreamIdType::LMS:
+      return &lms_streams[device_index][si];
     default:
       AT_ASSERTM(
           0,
@@ -367,6 +403,20 @@ void setCurrentCUDAStream(CUDAStream stream) {
   auto ptr = CUDAStream_internals(stream);
   AT_ASSERT(ptr);
   current_streams[ptr->device_index] = ptr;
+}
+
+CUDAStream getLMSCUDAStream(DeviceIndex device_index) {
+  initCUDAStreamsOnce();
+  if (device_index == -1)
+    device_index = current_device();
+  check_gpu(device_index);
+
+  // Initializes the LMS stream pool (once)
+  std::call_once(
+      device_flags_lms[device_index], initDeviceLMSStreamState, device_index);
+
+  const auto idx = get_idx(lms_counters[device_index]);
+  return CUDAStream_fromInternals(&lms_streams[device_index][idx]);
 }
 
 std::ostream& operator<<(std::ostream& stream, const CUDAStream& s) {